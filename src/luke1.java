8b547a6f7d7b0aa2e3bbfcaf2be468ffd8539238
rikkeaas <rikrikaas@gmail.com>
2019-12-01 10:31:06 -0500

    Luke 1 ferdig

--- src/luke1.java
+++ src/luke1.java
@@ -9,19 +9,35 @@ public class luke1 {
         File file = new File("src/luke1.txt");
         Scanner in = new Scanner(file);
         int total = 0;
+        int totalAccumulated = 0;
 
         while(in.hasNextLine()) {
             int nb = Integer.parseInt(in.nextLine());
             int fuel = calculateFuel(nb);
             total += fuel;
+            int fuelAccumulated = calculateFuelAccumulated(nb);
+            totalAccumulated += fuelAccumulated;
         }
 
         System.out.println(total);
+        System.out.println(totalAccumulated);
     }
 
     private static int calculateFuel(int nb) {
         return (nb / 3) - 2;
 
+    }
+
+    private static int calculateFuelAccumulated(int nb) {
+        int accumulatedFuel = 0;
+        int currFuel = calculateFuel(nb);
+
+        while (currFuel > 0) {
+            accumulatedFuel += currFuel;
+            currFuel = calculateFuel(currFuel);
+        }
+
+        return accumulatedFuel;
     }
 
 
